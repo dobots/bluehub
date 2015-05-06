c49641142f0bfc0132cb62c64e67a903cb1d8330
Anne van Rossum <anne@dobots.nl>
2015-05-06 21:46:08 +0200

    Only get installed apps

--- hub.js
+++ hub.js
@@ -1,15 +1,47 @@
 var http = require('http');
-
 var fs = require('fs');
 
-http.createServer(function (request, response) {
+var express = require('express');
+var app = express();
+
+//http.createServer(function (request, response) {
+app.get('/', function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
+	listmodules(function(err, res) {
+		if (err !== null) {
+			console.log(err);
+			response.end(err);
+		} else {
+			console.log(res);
+			response.end(res);
+		}
+	});
+
+	/*
    readdir('/data/data', function(err, res) {
       if (err) return;
       console.log(res);
       response.end(res);
-   });
+   }); */
 }).listen(8124);
+
+var spawn = require('child_process').spawn;
+
+function listmodules(callback) {
+	var child = spawn('pm', ['list', 'packages', '-f']);
+	var text = '';
+	child.stdout.on('data', function(chunk) {
+		text = text.concat(chunk);
+	});
+	child.on('close', function(code) {
+		var err = null;
+		if (code != 0) {
+			err = code;
+		}
+		callback(err, text);
+	});
+}
+
 
 function readdir(dir, res) {
    fs.readdir(dir, function(err, files) {
